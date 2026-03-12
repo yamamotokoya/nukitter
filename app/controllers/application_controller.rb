67fdf6d213168da86a48f2e4c1026b242a6d338c
Koya Yamamoto <koyayamamoto@KoyanoMacBook-Air.local>
2026-03-12 15:47:56 +0900

    deleted allow_browser: :modern from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
-
+  # allow_browser versions: :modern 
+  
   stale_when_importmap_changes
 
   helper_method :current_user, :logged_in?
