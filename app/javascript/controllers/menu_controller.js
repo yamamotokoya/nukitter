8306fd033d2f6ab758d9799dd54d75cb3ebb64a0
Koya Yamamoto <koyayamamoto@KoyanoMacBook-Air.local>
2026-03-12 16:05:17 +0900

    menu_controller.jsを修正

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -12,20 +12,35 @@ export default class extends Controller {
     }
   }
 
-  open() {
-    // メニューを出す
-    this.contentTarget.classList.remove("-translate-x-full")
-    this.contentTarget.classList.add("translate-x-0")
+  // open() {
+  //   // メニューを出す
+  //   this.contentTarget.classList.remove("-translate-x-full")
+  //   this.contentTarget.classList.add("translate-x-0")
     
-    // オーバーレイを出す
-    this.overlayTarget.classList.remove("hidden")
-    setTimeout(() => {
-      this.overlayTarget.classList.remove("opacity-0")
-    }, 10)
+  //   // オーバーレイを出す
+  //   this.overlayTarget.classList.remove("hidden")
+  //   setTimeout(() => {
+  //     this.overlayTarget.classList.remove("opacity-0")
+  //   }, 10)
 
-    // 背面のスクロール禁止
-    document.body.classList.add("overflow-hidden")
-  }
+  //   // 背面のスクロール禁止
+  //   document.body.classList.add("overflow-hidden")
+  // }
+
+  // open() メソッドの中身を以下のように調整
+open() {
+  this.overlayTarget.classList.remove("hidden")
+  
+  // ブラウザに「描画（Reflow）」を強制させるための1行を追加
+  this.overlayTarget.offsetHeight 
+
+  this.contentTarget.classList.remove("-translate-x-full")
+  this.contentTarget.classList.add("translate-x-0")
+  this.overlayTarget.classList.add("opacity-100")
+  this.overlayTarget.classList.remove("opacity-0")
+  document.body.classList.add("overflow-hidden")
+}
+
 
   close() {
     // メニューを隠す
