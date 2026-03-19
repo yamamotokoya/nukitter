970f814247da6a80acedd3520735176f6b72e67b
Koya Yamamoto <koyayamamoto@KoyanoAir.lan>
2026-03-19 15:51:21 +0900

    add config.active_storage.analyze_active_job = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.active_storage.analyze_active_job = false
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
